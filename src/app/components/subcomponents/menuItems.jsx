f9f6098589848be88df4286e4d8c50c7cff06e4a
vayania06 <vayania06@gmail.com>
2024-02-19 14:40:14 +0000

    just amde buttons bigger on mobile

--- src/app/components/subcomponents/menuItems.jsx
+++ src/app/components/subcomponents/menuItems.jsx
@@ -12,11 +12,11 @@ export default function MenuItems({item, price, setItems, id}) {
             <div className="w-4/5 flex justify-center text-center ">
                 <span className="text-4xl xs:max-sm:text-lg">{item}</span>    
             </div>
-            <div className="w-1/12 flex justify-center">
-                <div className="flex text-3xl bg-accent px-3 rounded-xl justify-center items-center xs:max-sm:text-lg">
-                    <button onClick={() => setItems(prevState => [...prevState, {id}])}> + </button>
+            <div className="w-1/12 flex justify-center xs:max-sm:w-6/12 h-8">
+                <div className="flex text-3xl bg-accent px-3 rounded-xl justify-center items-center xs:max-sm:text-2xl flex-1 justify-evenly">
+                    <button className="xs:max-sm:flex w-1/2 justify-center" onClick={() => setItems(prevState => [...prevState, {id}])}> + </button>
                     <div className="w-0.5 bg-text/50 h-full mx-1.5"></div>
-                    <button onClick={() => setItems(prevState => prevState.filter((item, index, arr) => {
+                    <button className="xs:max-sm:flex w-1/2 justify-center" onClick={() => setItems(prevState => prevState.filter((item, index, arr) => {
                             // Find the index of the item to remove
                             const itemToRemoveIndex = arr.findIndex(i => i.id === id);
                             // Remove the item if its index matches the current index

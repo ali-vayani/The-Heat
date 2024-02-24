97c985ec7547e92ef2dd0b4c4f022d1cfd376675
vayania06 <vayania06@gmail.com>
2024-02-24 21:33:50 +0000

    i think i fixed font issue

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
       <html lang="en">
-        <body >{children}</body>
+        <body className={sora.className}>{children}</body>
       </html>
     );
   }

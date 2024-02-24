6f6778c58da3988b635e9ea94f75c89bd0c23732
Omkar Rayudu <omkarrayudu@gmail.com>
2024-02-23 20:32:40 -0600

    login page looks better now

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -3,12 +3,15 @@ import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { Router, useRouter } from "next/navigation";
 import { FIREBASE_APP } from "../../../firebaseConfig";
 import { useState } from "react";
+import Image from "next/image";
+
 
 export default function Home() {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const router = useRouter();
+    const [loginError, setLoginError] = useState(' ');
 
     const auth = getAuth();
 
@@ -20,31 +23,41 @@ export default function Home() {
             router.push('/admin')
         })
         .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
+            setLoginError('Incorrect email or password');
         });
     }
 
     return (
-        <div className="flex flex-col flex-1 w-1/2 items-center justify-center ml-12 mr-6 mt-4 pb-8">
+        
+        <div className="flex flex-col flex-1 w-full items-center justify-center">
+            <Image
+              src="/TheHeatLogo.png"
+              alt="The Heat Logo"
+              width={750}
+              height={240}
+              priority
+            />
+            <div className="flex flex-col w-1/2 items-center justify-center m-10">
+                <div className=" flex text-red-500 text-xl mb-4">{loginError}</div>
                 <div className="flex bg-primary flex-col rounded-xl my-2 w-full">
                     <input
-                    type="text"
-                    className="text-4xl bg-transparent placeholder-text p-4"
-                    placeholder="Name"
-                    onChange={(e) => setEmail(e.target.value)}
-                    />
+                        type="text"
+                        className="text-4xl bg-transparent placeholder-text p-4"
+                        placeholder="Email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        />
                 </div>
                 <div className="flex bg-primary flex-col rounded-xl my-2 w-full">
-                    <input
-                    type="text"
-                    className="text-4xl bg-transparent placeholder-text p-4"
-                    placeholder="Name"
-                    onChange={(e) => setPassword(e.target.value)}
-                    />
+                        <input
+                            type="password"
+                            className="text-4xl bg-transparent placeholder-text p-4"
+                            placeholder="Password"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
                 </div>
                 <div className="flex justify-center flex-1 bg-primary w-full rounded-xl my-2">
-                <button onClick={handleSignIn} className="text-7xl">Login</button>
+                    <button onClick={handleSignIn} className="text-5xl w-full p-4">Login</button>
+                </div>
             </div>
         </div>
     )

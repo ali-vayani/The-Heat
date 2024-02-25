ccefe8cb0e800ae8dd74788a23f159b9afa495b6
Omkar Rayudu <omkarrayudu@gmail.com>
2024-02-24 19:32:57 -0600

    mobile optimization is done

--- src/app/components/ordersTable.jsx
+++ src/app/components/ordersTable.jsx
@@ -11,24 +11,24 @@ function OrdersTable({ orders, updateOrder }) {
     <table className="w-8/12 leading-normal mb-12 xs:max-sm:w-11/12">
       <thead>
         <tr>
-          <th className="px-5 py-3 border-b-2 border-gray-200 bg-background text-left text-xs font-semibold text-text uppercase tracking-wider xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xs">Name</th>
-          <th className="px-5 py-3 border-b-2 border-gray-200 bg-background text-left text-xs font-semibold text-text uppercase tracking-wider xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xs">Time</th>
-          <th className="px-5 py-3 border-b-2 border-gray-200 bg-background text-left text-xs font-semibold text-text uppercase tracking-wider xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xs">How to Eat</th>
-          <th className="px-5 py-3 border-b-2 border-gray-200 bg-background text-left text-xs font-semibold text-text uppercase tracking-wider xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xs">Payment Method</th>
-          <th className="px-5 py-3 border-b-2 border-gray-200 bg-background text-left text-xs font-semibold text-text uppercase tracking-wider xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xs">Paid?</th>
-          <th className="px-5 py-3 border-b-2 border-gray-200 bg-background text-left text-xs font-semibold text-text uppercase tracking-wider xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xs">Order Items</th>
-          <th className="px-5 py-3 border-b-2 border-gray-200 bg-background text-left text-xs font-semibold text-text uppercase tracking-wider xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xs">Total</th>
-          <th className="px-5 py-3 border-b-2 border-gray-200 bg-background text-left text-xs font-semibold text-text uppercase tracking-wider xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xs">Extra Info</th>
+          <th className="px-5 py-3 border-b-2 border-gray-200 bg-background text-left text-xs font-semibold text-text uppercase tracking-wider xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xxs">Name</th>
+          <th className="px-5 py-3 border-b-2 border-gray-200 bg-background text-left text-xs font-semibold text-text uppercase tracking-wider xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xxs">Time</th>
+          <th className="px-5 py-3 border-b-2 border-gray-200 bg-background text-left text-xs font-semibold text-text uppercase tracking-wider xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xxs">How to Eat</th>
+          <th className="px-5 py-3 border-b-2 border-gray-200 bg-background text-left text-xs font-semibold text-text uppercase tracking-wider xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xxs">Payment Method</th>
+          <th className="px-5 py-3 border-b-2 border-gray-200 bg-background text-left text-xs font-semibold text-text uppercase tracking-wider xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xxs">Paid?</th>
+          <th className="px-5 py-3 border-b-2 border-gray-200 bg-background text-left text-xs font-semibold text-text uppercase tracking-wider xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xxs">Order Items</th>
+          <th className="px-5 py-3 border-b-2 border-gray-200 bg-background text-left text-xs font-semibold text-text uppercase tracking-wider xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xxs">Total</th>
+          <th className="px-5 py-3 border-b-2 border-gray-200 bg-background text-left text-xs font-semibold text-text uppercase tracking-wider xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xxs">Extra Info</th>
         </tr>
       </thead>
       <tbody>
         {orders.map((order) => (
           <tr key={order['id']} className="border-b border-gray-200">
-            <td className="px-5 py-5 bg-primary text-sm xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xs">{order['name']}</td>
-            <td className="px-5 py-5 bg-primary text-sm xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xs">{`${order['hour']}:${String(order['minute']).padStart(2, '0')}`}</td>
-            <td className="px-5 py-5 bg-primary text-sm xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xs">{order['howEat']}</td>
-            <td className="px-5 py-5 bg-primary text-sm xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xs">{order['howPay']}</td>
-            <td className="px-5 py-5 bg-primary text-sm xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xs">
+            <td className="px-5 py-5 bg-primary text-sm xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xxs">{order['name']}</td>
+            <td className="px-5 py-5 bg-primary text-sm xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xxs">{`${order['hour']}:${String(order['minute']).padStart(2, '0')}`}</td>
+            <td className="px-5 py-5 bg-primary text-sm xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xxs">{order['howEat']}</td>
+            <td className="px-5 py-5 bg-primary text-sm xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xxs">{order['howPay']}</td>
+            <td className="px-5 py-5 bg-primary text-sm xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xxs">
               <input
                 type="checkbox"
                 checked={order.paid}
@@ -36,7 +36,7 @@ function OrdersTable({ orders, updateOrder }) {
                 
               />
             </td>
-            <td className="px-5 py-5 bg-primary text-sm xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xs">
+            <td className="px-5 py-5 bg-primary text-sm xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xxs">
               <ul>
                 {order['order'].map((item, index) => {
                   //Assuming 'item' is always a string for simplicity
@@ -44,8 +44,8 @@ function OrdersTable({ orders, updateOrder }) {
                 })}
               </ul>
             </td>
-            <td className="px-5 py-5 bg-primary text-sm xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xs">${order['total']}</td>
-            <td className="px-5 py-5 bg-primary text-sm xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xs">{order['extraInfo']}</td>
+            <td className="px-5 py-5 bg-primary text-sm xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xxs">${order['total']}</td>
+            <td className="px-5 py-5 bg-primary text-sm xs:max-sm:px-0 xs:max-sm:text-center xs:max-sm:text-xxs">{order['extraInfo']}</td>
           </tr>
         ))}
       </tbody>
--- tailwind.config.js
+++ tailwind.config.js
@@ -32,6 +32,21 @@ module.exports = {
       'lg': '1024px',
       'xl': '1280px',
     },
+    fontSize: {
+      'xxs': '0.5rem',
+      'sm': '0.875rem',    // 14px
+      'base': '1rem',      // 16px
+      'lg': '1.125rem',    // 18px
+      'xl': '1.25rem',     // 20px
+      '2xl': '1.5rem',     // 24px
+      '3xl': '1.875rem',   // 30px
+      '4xl': '2.25rem',    // 36px
+      '5xl': '3rem',       // 48px
+      '6xl': '4rem', 
+      '7xl': '4.5rem',
+      '8xl': '6rem',
+      '9xl': '8rem',
+    }
   },
   plugins: [],
 };
